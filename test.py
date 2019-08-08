97922150a5be6633a822f6eac6f820ba54ec5d27
a2323669a <a2323669a@sina.com>
2019-08-08 19:15:20 +0800

    re-train-1

--- test.py
+++ test.py
@@ -1,2 +1,28 @@
-import tensorflow as tf
-print(tf.__version__)
+#%%
+
+import pandas as pd
+df = pd.read_csv("./data/test_conv.csv")
+#%%
+id_list = df.iloc[:,0].values.reshape(-1,)
+df.drop(labels='id',axis=1,inplace=True)
+test_x = df.values.reshape((-1,48,48,1))
+
+#%%
+
+import keras
+
+model :keras.Sequential = keras.models.load_model('./h5/early_best.h5')
+pred = model.predict(test_x,verbose=2)
+
+#%%
+import numpy as np
+pred = np.argmax(pred,axis=1)
+#%%
+
+pred = pred.tolist()
+id_list = id_list.tolist()
+df_result = pd.DataFrame({'id':id_list,'label':pred})
+
+
+#%%
+df_result.to_csv('./result/pred.csv',index=False)
